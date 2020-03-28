212f92822b99c2abed094ec111752a7caae73145
fake <fake@asdfasdfkjalsdfkjskaldfjsadkfjljasdlkfjsladkjfklasjldfjkasdf.com>
2020-03-28 12:19:01 -0400

    Removed bootstrap js from default view

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -12,7 +12,7 @@
         <div class="container">
             @yield('content')
         </div>
-        <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+        <!-- <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script> -->
     <footer class="navbar fixed-bottom" style="height: 40px; background-color: #007bff;">
         <div class="container">
             <a style="color: white;" href="{{ route('todo') }}">Make a suggestion</a>
