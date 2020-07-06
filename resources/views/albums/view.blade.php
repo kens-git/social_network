600389216261634788e0e2ec5d444776640d1642
fake <fake@asdfasdfkjalsdfkjskaldfjsadkfjljasdlkfjsladkjfklasjldfjkasdf.com>
2020-07-05 22:36:23 -0400

    Reverted album view to proper preview HTML

--- resources/views/albums/view.blade.php
+++ resources/views/albums/view.blade.php
@@ -9,49 +9,29 @@
             $comment_count = App\FileComment::where('file_id', $file->id)->count();
         @endphp
         <div style="object-fit: contain;" width="300" height="300">
-            @if(in_array(strtolower($file->extension), ['mov', 'webm', 'mp4']))
-                    <video width="300" height="300" preload="false" src="{{ route('albums.file',
-                        ['username' => $user->username,
-                        'id' => $file->album_id,
-                        'file_id' => $file->id]) }}" controls>
-                    </video>
-                    <a href="{{ route('albums.file.view',
-                        ['username' => $user->username,
-                        'id' => $file->album_id,
-                        'file_id' => $file->id]) }}">Video Comments</a>
-            @elseif(in_array(strtolower($file->extension), ['jpg', 'jpeg', 'png', 'gif']))
-                <a href="{{ route('albums.file.view',
-                        ['username' => $user->username,
-                        'id' => $file->album_id,
-                        'file_id' => $file->id]) }}" class="file-preview-link">
-                    <div>
-                        <img class="album-image-preview" src="{{ route('albums.file_preview',
-                        ['username' => $user->username,
-                        'id' => $file->album_id,
-                        'file_id' => $file->id]) }}"/>
-                        <br/>
-                        <p class="file-preview-comment-count">{{ $comment_count }} comments</p>
-                        @if($file->user_id == Auth::user()->id)
-                            <form action="{{ route('set_profile_photo', ['id' => $file->id]) }}" method="get">
-                                @csrf()
-                                <button class="set-as-link" type="submit">Set As Profile Picture</button>
-                            </form>
-                            <form action="{{ route('set_cover_photo', ['id' => $file->id]) }}" method="get">
-                                @csrf()
-                                <button class="set-as-link" type="submit">Set As Cover Picture</button>
-                            </form>
-                        @endif
-                    </div>
-                </a>
-            @else
-                <a href="{{ route('albums.file.view',
-                        ['username' => $user->username,
-                        'id' => $file->album_id,
-                        'file_id' => $file->id]) }}">
-                    <img width="300" src="{{ asset('icons/file_icon.png') }}">
-                    <p>{{ $file->name }}</p>
-                </a>
-            @endif
+            <a href="{{ route('albums.file.view',
+                    ['username' => $user->username,
+                    'id' => $file->album_id,
+                    'file_id' => $file->id]) }}" class="file-preview-link">
+                <div>
+                    <img class="album-image-preview" src="{{ route('albums.file_preview',
+                    ['username' => $user->username,
+                    'id' => $file->album_id,
+                    'file_id' => $file->id]) }}"/>
+                    <br/>
+                    <p class="file-preview-comment-count">{{ $comment_count }} comments</p>
+                    @if($file->user_id == Auth::user()->id)
+                        <form action="{{ route('set_profile_photo', ['id' => $file->id]) }}" method="get">
+                            @csrf()
+                            <button class="set-as-link" type="submit">Set As Profile Picture</button>
+                        </form>
+                        <form action="{{ route('set_cover_photo', ['id' => $file->id]) }}" method="get">
+                            @csrf()
+                            <button class="set-as-link" type="submit">Set As Cover Picture</button>
+                        </form>
+                    @endif
+                </div>
+            </a>
         </div>
     @endforeach
 @stop
