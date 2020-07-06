ffc6fd48d44d1c0cc92ecdb4b00704bf8bfb345f
fake <fake@asdfasdfkjalsdfkjskaldfjsadkfjljasdlkfjsladkjfklasjldfjkasdf.com>
2020-07-05 22:16:13 -0400

    Updated mobile styles

--- resources/views/templates/messages/send_form.blade.php
+++ resources/views/templates/messages/send_form.blade.php
@@ -1,9 +1,12 @@
-<form id="send-message-form" method="post" action="{{ route('messages', ['username' => $user->username]) }}">
+<form method="post" action="{{ route('messages', ['username' => $user->username]) }}">
     @csrf()
-    @error('message')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <textarea name="message"></textarea>
-    <br/>
-    <input type="submit" value="Send Message">
+    <div class="form-group">
+        @error('message')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <textarea name="message"></textarea>
+    </div>
+    <div class="form-group">
+        <input type="submit" value="Send Message">
+    </div>
 </form>
